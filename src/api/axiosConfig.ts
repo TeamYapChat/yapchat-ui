479e99c5ea144dbd39784f07a1d4b4c675f93410
KhiemNguyen15 <khiemsa15@gmail.com>
2025-02-14 16:25:07 -0600

    chore: Set base URL for Axios

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -3,51 +3,51 @@ import { Store } from '@reduxjs/toolkit';
 import { RootState } from '../features/store';
 import { logout } from '../features/auth/authSlice';
 
-const baseUrl = import.meta.env.VITE_BASE_URL;
+const baseUrl = "https://api.yapchat.xyz";
 
-let store : Store<RootState>;
+let store: Store<RootState>;
 
 export const injectStore = (newStore: Store<RootState>) => {
-    store = newStore;
+  store = newStore;
 }
 
 const axiosInstance = axios.create({
-    baseURL: baseUrl,
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: baseUrl,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem('token');
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-  );
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 
 axiosInstance.interceptors.response.use(
-(response) => {
+  (response) => {
     const data = response.data;
 
     return data;
-},
-(error) => {
-    
+  },
+  (error) => {
+
     if (error.response.status === 401) {
-    // Clear token and Authenticated State
-    store.dispatch(logout());
+      // Clear token and Authenticated State
+      store.dispatch(logout());
     }
 
     return Promise.reject(error);
-}
+  }
 );
 
 export default axiosInstance;
