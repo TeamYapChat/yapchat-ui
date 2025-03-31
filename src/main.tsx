c09634062a6a3d6ba3ee6423f31be8c6d06045f0
KhiemNguyen15 <khiemsa15@gmail.com>
2025-03-31 09:47:32 -0500

    fix: Use config.json in place of environment variables

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,15 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./features/store.ts";
-import { ClerkProvider } from '@clerk/clerk-react'
+import { ClerkProvider } from "@clerk/clerk-react";
 
 import { injectStore } from "./api/axiosConfig.js";
 
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+async function loadConfig() {
+  const res = await fetch("/config.json");
+  const config = await res.json();
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  return String(config.CLERK_PUBLISHABLE_KEY);
 }
 
 injectStore(store);
@@ -22,7 +22,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <ClerkProvider publishableKey={await loadConfig()}>
           <App />
         </ClerkProvider>
       </Provider>
