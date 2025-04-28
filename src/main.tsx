c21578642b4d62eb62fe9869451cc3e6a92d7e17
KhiemNguyen15 <khiemsa15@gmail.com>
2025-04-28 13:32:12 -0500

    chore: Use Clerk publishable key from config.json

--- src/main.tsx
+++ src/main.tsx
@@ -18,9 +18,7 @@ async function initApp() {
   createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Provider store={store}>
-        <ClerkProvider
-          publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY!}
-        >
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
           <App />
         </ClerkProvider>
       </Provider>
